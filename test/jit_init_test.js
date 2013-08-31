16d201645307b2ed78feae92ee6704204a2c50ba
Ignazio Setti <ignazio.setti@gmail.com>
2013-08-31 16:20:06 +0200

    Refactor test to use new repo api & remove useless test

--- test/jit_init_test.js
+++ test/jit_init_test.js
@@ -3,10 +3,11 @@
 var fs = require('fs-extra'),
     path = require('path'),
     exec = require('child_process').exec,
-    Repo = require('../lib/common/repo');
+    Repo = require('../lib/common/repo'),
+    storage = require('../lib/storage/filesystem');
 
 var git_folder = 'git-repo';
-var jit_folder = 'jit-repo';
+var jit_folder = 'jit-repo/.git';
 
 /**
 *
@@ -18,8 +19,8 @@ exports.testDefaultDirectoryTree = function(test) {
   exec('git init ' + git_folder, continueTest);
 
   function continueTest() {
-    var repo = new Repo();
-    repo.init({gitDir: jit_folder}, function(err) {
+    var repo = new Repo(jit_folder, storage);
+    repo.init(function(err) {
       var git_tree = fs.readdirSync(git_folder + '/.git');
       var jit_tree = fs.readdirSync(repo.root_dir);
 
@@ -31,43 +32,4 @@ exports.testDefaultDirectoryTree = function(test) {
       test.done();
     });
   }
-}
-
-
-/**
-*
-* Verify jit doesn't allow writing git folders if they're already there.
-* If they are there, it instantiates a repo from already existing folders.
-*
-**/
-
-exports.testJitInitLoad = function(test) {
-  var repo = new Repo();
-
-  function repo_init(callback) {
-    return repo.init_new({gitDir: jit_folder}, callback);
-  }
-
-  function repo_load(callback) {
-    return repo.init_from_dir({gitDir: jit_folder}, callback);
-  }
-
-  repo_init(function(err) {
-    test.equal(err, undefined);
-
-    if(!err) {
-      repo_load(function(err) {
-        test.equal(err, undefined);
-
-        if(!err) {
-          repo_init(function(err) {
-            test.notEqual(err, undefined);
-
-            fs.removeSync(jit_folder);
-            test.done();
-          });
-        }
-      });
-    }
-  });
 }
