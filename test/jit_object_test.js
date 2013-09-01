530698074b847ab450791df5296ccaddfcf27881
Ignazio Setti <ignazio.setti@gmail.com>
2013-09-01 14:13:24 +0200

    Renamed blob test to object test

--- test/jit_object_test.js
+++ test/jit_object_test.js
@@ -4,7 +4,7 @@ var fs = require('fs-extra'),
     path = require('path'),
     exec = require('child_process').exec,
     Repo = require('../lib/common/repo'),
-    Blob = require('../lib/common/blob'),
+    GitObject = require('../lib/common/git_object'),
     storage = require('../lib/storage/filesystem');
 
 var git_folder = 'git-repo';
@@ -12,8 +12,8 @@ var jit_folder = 'jit-repo';
 
 /**
 *
-* Test that git and jit agree on setting blob keys.
-* Pay attention: blob content created on jit needs to ALWAYS have a \n
+* Test that git and jit agree on setting object keys.
+* Pay attention: object content created on jit needs to ALWAYS have a \n
 * at the end when reading from stdin but setting it from js
 * (stdin always present a newline char at the end).
 *
@@ -23,8 +23,8 @@ exports.testShaKeyFromStdin = function(test) {
   var content = 'hello boys, how are you?';
 
   exec('echo "' + content + '" | git hash-object --stdin', function(err, stdout, stderr) {
-    var blob = new Blob(content + '\n');
-    test.equal(blob.key, stdout.replace(/[\n]/g, ''));
+    var object = new GitObject(content + '\n');
+    test.equal(object.key, stdout.replace(/[\n]/g, ''));
     test.done();
   });
 }
@@ -37,10 +37,13 @@ exports.testShaKeyFromFile = function(test) {
     fs.readFile(file_path, 'utf8', function(err, data) {
       var repo = new Repo(jit_folder + '/.git', storage);
       repo.init(function(err) {
-        repo.write_blob({content: data}, function(err, blob) {
+        repo.write_object({content: data}, function(err, object) {
           test.equal(err, null);
-          test.equal(blob.key, stdout.replace(/[\n]/g, ''));
+          test.equal(object.key, stdout.replace(/[\n]/g, ''));
+
           fs.removeSync('gist');
+          fs.removeSync(jit_folder);
+
           test.done();
         });
       });
